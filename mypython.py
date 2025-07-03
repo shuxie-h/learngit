0d16009293de20322a11fe73e607ef8fb942b0a9
shuxie-h <1211207500@qq.com>
2025-07-03 21:46:46 +0800

    huxiaojun的第一次提交

---
+++ mypython.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    start = "2025-04-01"
+    print(start)
+    end = "2025-05-01"
+    print(end)
+    action_cz = OnlineReport(
+        start_date=start,
+        end_date=end,
+        jira_assignee="储值",
+        period="month",
+        dimension="jira_module",
+        data_type="1",
+        add_document=False,
+        send_message=True,
+        receive_message_name="huxiaojun_hu",
+        receive_message_type="private_chat",
+        receive_message_addr="https://open.feishu.cn/open-apis/bot/v2/hook/c1d9bde3-b3ba-45a4-bb07-8f658299fdd3",
+    )
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(action_cz.report_data_main())
+
+
+    
